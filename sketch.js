8a233e2d1f664f1b1e6853aead073e3869594828
seungeon <seungeon.k@gmail.com>
2014-12-04 14:15:19 +0900

    dd

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup()
+{
+    createCanvas(1280,908);
+    bg = loadImage("bg.png");
+
+
+    img = loadImage("img1.png");
+    img2 = loadImage("img2.png");
+}
+
+function draw()
+{
+    background(bg);
+    
+
+    fill(237,112,50);
+    noStroke();
+}
