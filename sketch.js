f164d76b2e950dae7dd37a391c6793968c828daf
seungeon <seungeon.k@gmail.com>
2014-12-04 14:22:15 +0900

    dddd

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+var loadingImage = 1;
+
+
 function setup()
 {
     createCanvas(1280,908);
@@ -13,6 +16,23 @@ function draw()
     background(bg);
     
 
+
     fill(237,112,50);
     noStroke();
+
+    if(loadingImage === 1){
+    	image(img,0,0);
+    }else if(loadingImage === 2){
+    	image(img2,0,0);
+    }
+}
+
+function mousePressed(){
+	if(mouseX > 50 && mouseX <150 && mouseY > 50 && mouseY <150){
+		if(loadingImage === 1){
+			loadingImage = 2;
+		}else if(loadingImage ===2){
+			loadingImage =1;
+		}
+	}
 }
